6046541077fae2fc99afc4aa3889e3675b80507f
levelmoon <2205534711zy@gmail.com>
2023-05-19 08:41:44 +0800

    同步

---
+++ vins_estimator/src/utility/tic_toc.h
@@ -0,0 +1,40 @@
+/*******************************************************
+ * Copyright (C) 2019, Aerial Robotics Group, Hong Kong University of Science and Technology
+ * 
+ * This file is part of VINS.
+ * 
+ * Licensed under the GNU General Public License v3.0;
+ * you may not use this file except in compliance with the License.
+ *******************************************************/
+
+#pragma once
+
+#include <ctime>
+#include <cstdlib>
+#include <chrono>
+
+
+//测量时间间隔的工具类
+class TicToc
+{
+  public:
+    TicToc()
+    {
+        tic();
+    }
+
+    void tic()
+    {
+        start = std::chrono::system_clock::now();
+    }
+
+    double toc()
+    {
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        return elapsed_seconds.count() * 1000;
+    }
+
+  private:
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+};
---
+++ vins_estimator/src/utility/utility.h
@@ -0,0 +1,166 @@
+/*******************************************************
+ * Copyright (C) 2019, Aerial Robotics Group, Hong Kong University of Science and Technology
+ * 
+ * This file is part of VINS.
+ * 
+ * Licensed under the GNU General Public License v3.0;
+ * you may not use this file except in compliance with the License.
+ *******************************************************/
+
+#pragma once
+
+#include <cmath>
+#include <cassert>
+#include <cstring>
+#include <eigen3/Eigen/Dense>
+
+class Utility
+{
+  public:
+    /*
+        Derived 是一个模板参数，代表的是任何可以转换成 Eigen::MatrixBase 的类型。这种方式允许函数接受任何维度和大小的 Eigen 矩阵或向量。
+        因此，Derived 其实并不是一个具体的类型，而是一个抽象的模板参数，它代表的具体类型取决于你如何使用这个函数。
+
+        这个函数的参数 const Eigen::MatrixBase<Derived> &theta 是一个模板化的 Eigen 对象的引用。
+        这就意味着这个函数可以接受任何 Eigen 矩阵或向量类型的 theta，只要它们可以被看作是 Eigen::MatrixBase 类型的对象。
+        而 typename Derived::Scalar 是模板参数 Derived 的标量类型。
+        
+        换句话说，如果你使用一个 Eigen::Vector3d（它是 Eigen::MatrixBase 的派生类型）来调用这个函数，
+        那么 Derived 就是 Eigen::Vector3d，Derived::Scalar 就是 double，这是 Eigen::Vector3d 的元素类型。
+    */
+    template <typename Derived>
+    static Eigen::Quaternion<typename Derived::Scalar> deltaQ(const Eigen::MatrixBase<Derived> &theta)
+    {
+        typedef typename Derived::Scalar Scalar_t;
+
+        Eigen::Quaternion<Scalar_t> dq;
+        Eigen::Matrix<Scalar_t, 3, 1> half_theta = theta;
+
+        //对旋转向量（角速度）进行二分，这是因为在四元数小角度近似的公式中需要用到角度的一半
+        //在小角度旋转的情况下，w接近1，而x、y、z分别是角速度的一半。
+        half_theta /= static_cast<Scalar_t>(2.0);
+        dq.w() = static_cast<Scalar_t>(1.0);
+        dq.x() = half_theta.x();
+        dq.y() = half_theta.y();
+        dq.z() = half_theta.z();
+        return dq;
+    }
+
+    template <typename Derived>
+    static Eigen::Matrix<typename Derived::Scalar, 3, 3> skewSymmetric(const Eigen::MatrixBase<Derived> &q)
+    {
+        Eigen::Matrix<typename Derived::Scalar, 3, 3> ans;
+        ans << typename Derived::Scalar(0), -q(2), q(1),
+            q(2), typename Derived::Scalar(0), -q(0),
+            -q(1), q(0), typename Derived::Scalar(0);
+        return ans;
+    }
+
+    template <typename Derived>
+    static Eigen::Quaternion<typename Derived::Scalar> positify(const Eigen::QuaternionBase<Derived> &q)
+    {
+        //printf("a: %f %f %f %f", q.w(), q.x(), q.y(), q.z());
+        //Eigen::Quaternion<typename Derived::Scalar> p(-q.w(), -q.x(), -q.y(), -q.z());
+        //printf("b: %f %f %f %f", p.w(), p.x(), p.y(), p.z());
+        //return q.template w() >= (typename Derived::Scalar)(0.0) ? q : Eigen::Quaternion<typename Derived::Scalar>(-q.w(), -q.x(), -q.y(), -q.z());
+        return q;
+    }
+
+    template <typename Derived>
+    static Eigen::Matrix<typename Derived::Scalar, 4, 4> Qleft(const Eigen::QuaternionBase<Derived> &q)
+    {
+        Eigen::Quaternion<typename Derived::Scalar> qq = positify(q);
+        Eigen::Matrix<typename Derived::Scalar, 4, 4> ans;
+        ans(0, 0) = qq.w(), ans.template block<1, 3>(0, 1) = -qq.vec().transpose();
+        ans.template block<3, 1>(1, 0) = qq.vec(), ans.template block<3, 3>(1, 1) = qq.w() * Eigen::Matrix<typename Derived::Scalar, 3, 3>::Identity() + skewSymmetric(qq.vec());
+        return ans;
+    }
+
+    template <typename Derived>
+    static Eigen::Matrix<typename Derived::Scalar, 4, 4> Qright(const Eigen::QuaternionBase<Derived> &p)
+    {
+        Eigen::Quaternion<typename Derived::Scalar> pp = positify(p);
+        Eigen::Matrix<typename Derived::Scalar, 4, 4> ans;
+        ans(0, 0) = pp.w(), ans.template block<1, 3>(0, 1) = -pp.vec().transpose();
+        ans.template block<3, 1>(1, 0) = pp.vec(), ans.template block<3, 3>(1, 1) = pp.w() * Eigen::Matrix<typename Derived::Scalar, 3, 3>::Identity() - skewSymmetric(pp.vec());
+        return ans;
+    }
+
+    /**
+     * 旋转矩阵计算Yaw、Pitch、Roll姿态角
+    */
+    static Eigen::Vector3d R2ypr(const Eigen::Matrix3d &R)
+    {
+        Eigen::Vector3d n = R.col(0);
+        Eigen::Vector3d o = R.col(1);
+        Eigen::Vector3d a = R.col(2);
+
+        Eigen::Vector3d ypr(3);
+        double y = atan2(n(1), n(0));
+        double p = atan2(-n(2), n(0) * cos(y) + n(1) * sin(y));
+        double r = atan2(a(0) * sin(y) - a(1) * cos(y), -o(0) * sin(y) + o(1) * cos(y));
+        ypr(0) = y;
+        ypr(1) = p;
+        ypr(2) = r;
+
+        return ypr / M_PI * 180.0;
+    }
+
+    template <typename Derived>
+    static Eigen::Matrix<typename Derived::Scalar, 3, 3> ypr2R(const Eigen::MatrixBase<Derived> &ypr)
+    {
+        typedef typename Derived::Scalar Scalar_t;
+
+        Scalar_t y = ypr(0) / 180.0 * M_PI;
+        Scalar_t p = ypr(1) / 180.0 * M_PI;
+        Scalar_t r = ypr(2) / 180.0 * M_PI;
+
+        Eigen::Matrix<Scalar_t, 3, 3> Rz;
+        Rz << cos(y), -sin(y), 0,
+            sin(y), cos(y), 0,
+            0, 0, 1;
+
+        Eigen::Matrix<Scalar_t, 3, 3> Ry;
+        Ry << cos(p), 0., sin(p),
+            0., 1., 0.,
+            -sin(p), 0., cos(p);
+
+        Eigen::Matrix<Scalar_t, 3, 3> Rx;
+        Rx << 1., 0., 0.,
+            0., cos(r), -sin(r),
+            0., sin(r), cos(r);
+
+        return Rz * Ry * Rx;
+    }
+
+    static Eigen::Matrix3d g2R(const Eigen::Vector3d &g);
+
+    template <size_t N>
+    struct uint_
+    {
+    };
+
+    template <size_t N, typename Lambda, typename IterT>
+    void unroller(const Lambda &f, const IterT &iter, uint_<N>)
+    {
+        unroller(f, iter, uint_<N - 1>());
+        f(iter + N);
+    }
+
+    template <typename Lambda, typename IterT>
+    void unroller(const Lambda &f, const IterT &iter, uint_<0>)
+    {
+        f(iter);
+    }
+
+    template <typename T>
+    static T normalizeAngle(const T& angle_degrees) {
+      T two_pi(2.0 * 180);
+      if (angle_degrees > 0)
+      return angle_degrees -
+          two_pi * std::floor((angle_degrees + T(180)) / two_pi);
+      else
+        return angle_degrees +
+            two_pi * std::floor((-angle_degrees + T(180)) / two_pi);
+    };
+};
